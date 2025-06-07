9ef86828ba8f85c8f8605e4c3bbb09f9310db9a2
chethankumar-ai <chethankumarchethan25@gmail.com>
2025-06-07 22:26:51 +0530

    day11

---
+++ Day11/day11.py
@@ -0,0 +1,10 @@
+import datetime
+
+date1 = datetime.date(2025, 6, 7)
+date2 = datetime.date(2025, 7, 1)
+
+# Calculate difference
+date_diff = date2 - date1
+
+# Get the number of days
+print(f"Days between: {date_diff.days}")
