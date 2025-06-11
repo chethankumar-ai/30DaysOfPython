c5e314bdb6db5481fc5424220b4f277a0a480bf3
chethankumar-ai <chethankumarchethan25@gmail.com>
2025-06-11 18:55:52 +0530

    IDC 15

---
+++ Day15/day15.py
@@ -0,0 +1,20 @@
+def time(func):
+    def inner(*args, **kwargs):
+        import time
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Total time taken: {end - start:.6f} seconds")
+        return result
+    return inner
+
+@time
+def factorial(n):
+    def inner_fact(x):  # inner function does actual recursion
+        if x == 0 or x == 1:
+            return 1
+        return x * inner_fact(x - 1)
+    
+    return inner_fact(n)  # only outer call is timed
+
+factorial(5)
